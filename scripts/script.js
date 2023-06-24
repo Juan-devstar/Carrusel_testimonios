c25f2af5e9d079dd640a3b6a969708b3789278d1
Your Name <you@example.com>
2023-06-23 20:53:11 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) =>{
+   slider();
+   toggleSliderOnClick();
+});
+
+function slider() {
+  const $nextBtn = d.querySelector(".slider-btns .next"),
+    $prevBtn = d.querySelector(".slider-btns .prev"),
+    $slides = d.querySelectorAll(".slider-slide");
+
+  let i = 0;
+  d.addEventListener("click", (e) => {
+    if (e.target === $prevBtn) {
+      e.preventDefault();
+      $slides[i].classList.remove("active");
+      i--;
+
+      if (i < 0) {
+        i = $slides.length - 1;
+      }
+
+      $slides[i].classList.add("active");
+    }
+
+    if (e.target === $nextBtn) {
+      e.preventDefault();
+      $slides[i].classList.remove("active");
+      i++;
+
+      if (i > $slides.length - 1) {
+        i = 0;
+      }
+
+      $slides[i].classList.add("active");
+    }
+  });
+}
+
+function toggleSliderOnClick() {
+  const $whiteButton = d.querySelector('#boton-blanco');
+  const $slider = d.querySelector('.slider');
+
+  $whiteButton.addEventListener('click', () => {
+    if ($slider.style.display === 'none') {
+      $slider.style.display = 'block';
+    } else {
+      $slider.style.display = 'none';
+    }
+  });
+}
